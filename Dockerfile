a7267bd5bbb7d75ac5c2a570f7a60ed0962352e5
Olof <olof.jp.gunnarsson@gmail.com>
2016-10-11 11:21:18 +0200

    Working app

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM tomee:8-jre-1.7.4-jaxrs
+ADD target/javaee-1.0-SNAPSHOT.war /usr/local/tomee/webapps/javaee-1.0-SNAPSHOT.war
--- src/main/java/se/kits/javaee/App.java
+++ src/main/java/se/kits/javaee/App.java
@@ -1,7 +1,21 @@
 package se.kits.javaee;
 
-public class App {
-    public static void main(String[] args) {
 
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Stateless
+public class App {
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    @Path("/")
+    public String helloWorld() {
+        return "{\"result\": \"Hello world!\"}";
     }
+
 }
