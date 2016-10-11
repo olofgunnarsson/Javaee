879278cb0daf9d7da23d8931dcb4de1f89e7402d
Olof <olof.jp.gunnarsson@gmail.com>
2016-10-11 15:35:27 +0200

    Fix db

---
+++ src/main/java/se/kits/javaee/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package se.kits.javaee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("app")
+public class JAXRSConfiguration extends Application {
+
+}
